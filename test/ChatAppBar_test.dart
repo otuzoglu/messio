fff5121d7f88b43653278bec559d382cf6be300b
otuzoglu <otuzoglualper@gmail.com>
2021-05-02 04:46:51 +0300

    Day 4-5 v2

---
+++ test/ChatAppBar_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:messio/widgets/ChatAppBar.dart';
+
+void main() {
+  MaterialApp app = MaterialApp(
+    home: Scaffold(
+      body: ChatAppBar(),
+    ),
+  );
+  testWidgets('ChatAppBar UI Test', (WidgetTester tester) async {
+    await tester.pumpWidget(app);
+
+    expect(find.text('Alper OTUZOĞLU'), findsOneWidget);
+    expect(find.text('@infelix'), findsOneWidget);
+    expect(find.byType(IconButton), findsNWidgets(1));
+    expect(find.byType(CircleAvatar), findsOneWidget);
+  });
+}
